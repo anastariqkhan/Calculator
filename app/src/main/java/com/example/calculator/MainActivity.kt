f28e2d78e0b4d13a5bb22763f1872f3adee2484b
Anas Tariq <anastariqkhan@gmail.com>
2021-04-29 14:53:35 +0500

    initial commit

---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.calculator
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            val txtNumber = findViewById<EditText>(R.id.txt_number)
+            val age = txtNumber.text.toString().toInt()
+            if (age <= 25) {
+                Toast.makeText(this, "You are too young", Toast.LENGTH_SHORT).show()
+            } else if (age > 50) {
+                Toast.makeText(this, "You are too Old", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "You are eligible", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this, SecondActivity::class.java))
+            }
+        }
+    }
+}
